1d85eb4e1d5dd2f1533a4c759e8c190369ff5e40
Said Oudouane <saidoudouane@gmail.com>
2021-12-14 23:29:47 +0100

    Add mobile nav and home intro with styling on mobile

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,13 @@
 console.log("hello world");
+
+$(document).ready(function () {
+  listenToNavBtn();
+});
+
+const listenToNavBtn = () => {
+  const btn = $(".btns__nav");
+  btn.click(function () {
+    $(".navigation__mobile").toggleClass("navigation__mobile--open");
+  });
+  return btn;
+};
